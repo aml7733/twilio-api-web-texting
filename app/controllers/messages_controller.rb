382a4d0b0f70f049cb7198977291d43b6798bf9d
Adam <lucia.adam@gmail.com>
2018-02-05 00:43:58 -0500

    add initial views and controller actions, with routes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
+  def create
+    byebug
+    @message = Message.new(params.require(:message).permit(:from_name, :phone_number, :text))
+
+    if @message.save
+      redirect_to (MESSAGE_SENT_PATH), alert: "Message successfully saved."
+    else
+      render :new
+    end
+
+  end
+
+  def show
+  end
 end
