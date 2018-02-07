1bc3ba0f845a0b50f83c469f4fc82b5bbec08a07
Adam <lucia.adam@gmail.com>
2018-02-07 01:45:58 -0500

    add model & controller validations and send method logic

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,17 +4,29 @@ class MessagesController < ApplicationController
   end
 
   def create
-    byebug
     @message = Message.new(params.require(:message).permit(:name, :phone_number, :text))
 
-    if @message.save
-      redirect_to (MESSAGE_SENT_PATH), alert: "Message successfully saved."
+    if GlobalPhone.validate(@message.phone_number) && @message.save
+      send(@message.phone_number, @message.text)
+      redirect_to message_path(@message), alert: "Message successfully sent."
     else
-      render :new
+      render :new, alert: "There was a problem with the information you entered.  Please try again."
     end
 
   end
 
   def show
   end
+
+  private
+    def send(phone_number, text)
+      @twilio_number = ENV['TWILIO_PHONE_NUMBER']
+      @client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+
+      message = @client.account.messages.create(
+        from: @twilio_number,
+        to: phone_number,
+        body: text
+      )
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,3 @@
 class Message < ApplicationRecord
+  validates :name, :phone_number, :text, presence: true
 end
